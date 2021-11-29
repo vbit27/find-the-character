37c2ffc30fca51e77b2ff207885b80b0a5f87454
Vasil Bit <vasil.bituni@gmail.com>
2021-11-29 11:34:02 +0100

    Hide firebase  key

--- src/firebase_config.tsx
+++ src/firebase_config.tsx
@@ -2,7 +2,7 @@ import firebase from 'firebase/compat/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCdbG59PFDR2EDVUOyLMA7TfXhn0_umoHQ',
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: 'where-is-waldo-bb563.firebaseapp.com',
   projectId: 'where-is-waldo-bb563',
   storageBucket: 'where-is-waldo-bb563.appspot.com',
