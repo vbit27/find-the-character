37f10970dfb2d4e501000dc3c331b3455e8d723e
Vasil Bit <vasil.bituni@gmail.com>
2021-11-29 12:43:03 +0100

    Remove hours from timer

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,6 @@ export const formatTime = (time: number) => {
   const getSeconds = `0${Math.round(time % 60)}`.slice(-2);
   const minutes = Math.floor(time / 60);
   const getMinutes = `0${minutes % 60}`.slice(-2);
-  const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
 
-  return `${getHours}:${getMinutes}:${getSeconds}`;
+  return `${getMinutes}:${getSeconds}`;
 };
